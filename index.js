032e5cda1aa0d4a68dc5d0612ad36ce662cb6cb6
hassannaveed24 <hassannaveed24@gmail.com>
2020-05-29 19:55:03 +0500

    Fixed CORS by adding headers

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 const config = require('config');
@@ -10,41 +9,41 @@ const Joi = require('joi');
 const winston = require('winston');
 require('winston-mongodb');
 require('express-async-errors');
-
-app.use(cors());
-
+ 
+app.use((req, res, next)=>{
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "Origin, ");
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    next();
+});
+ 
 require('./startup/routes')(app);
-
-
+ 
+ 
 app.set('view engine','pug');
 app.set('views','./views');
-
-
+ 
+ 
 Joi.objectId = require('joi-objectid')(Joi);
-
+ 
 app.use(helmet());
 app.use(compression());
-
-// app.use((req, res, next)=>{
-//     // res.header("Access-Control-Allow-Origin", "*");
-//     // res.header("Access-Control-Allow-Headers", "Origin, ");
-//     res.setHeader("Access-Control-Allow-Origin","*");
-//     res.setHeader(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//         );
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-//     next();
-// });
-
+ 
+ 
+ 
 const db = config.get('db');
 mongoose.connect(db, {
      useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex: true})
     .then(() => winston.info('Connected to MongoDB..'));
-
+ 
     if(!config.get('jwtPrivateKey')){
         throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
     }
-
+ 
 const port = process.env.PORT || 3000;
-app.listen(port,()=> winston.info(`Listening on port: ${port}` ));
+app.listen(port,()=> winston.info(Listening on port: ${port} ));
