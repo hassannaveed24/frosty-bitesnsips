9b6e8912803d447336497acc34bfe108bdcd51ed
hassannaveed24 <hassannaveed24@gmail.com>
2020-06-01 03:56:49 +0500

    changing directory

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const home = require('../routes/home');
+
 const stock = require('../routes/stock');
 const product = require('../routes/product');
 const order = require('../routes/order');
@@ -14,14 +14,16 @@ module.exports = function(app){
     app.use(express.urlencoded({extended: true})); // 
     app.use(express.static('public'));
     app.use(helmet());
-    app.use(morgan('tiny'));  
+    app.use(morgan('tiny')); 
+    
 
-    app.use('/', home);
+    
     app.use('/api/stock', stock);
     app.use('/api/product', product);
     app.use('/api/order', order);
     app.use('/api/users', users);
     app.use('/api/auth', auth);    
+
 
     app.use(error);
 }
