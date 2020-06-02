ba891fb128557a7ef8aea91c443a871de7d13398
hassannaveed24 <hassannaveed24@gmail.com>
2020-06-02 23:34:47 +0500

    making addStock API

--- routes/addstock.js
+++ routes/addstock.js
@@ -8,13 +8,13 @@ router.put('/:id',async (req,res)=>{
     const {error} = validate(req.body);
     if(error)
         return res.status(400).send(error.details[0].message);
-        
+    
     const oldStock = await Stock.findOne();
-
+    const newquantity = parseFloat(oldStock.quantity)  + parseFloat(req.body.quantity);
     const stock = await Stock.findByIdAndUpdate(req.params.id,{
-        quantity: parseFloat(oldStock.quantity) - parseFloat(req.body.quantity)
+        quantity: newquantity
     },{new: true});
-  
+    
     if(!stock)
         return res.status(404).send('The stock with the given ID not found.');
 
--- routes/stock.js
+++ routes/stock.js
@@ -34,22 +34,7 @@ router.post('/', async(req, res) =>{
     res.send(stock);
 
 });
-router.put('/:id',async (req,res)=>{
-    const {error} = validate(req.body);
-    if(error)
-        return res.status(400).send(error.details[0].message);
-    
-    const oldStock = await Stock.findOne();
-    const newquantity = parseFloat(oldStock.quantity)  + parseFloat(req.body.quantity);
-    const stock = await Stock.findByIdAndUpdate(req.params.id,{
-        quantity: newquantity
-    },{new: true});
-    
-    if(!stock)
-        return res.status(404).send('The stock with the given ID not found.');
 
-    res.send(stock);
-});
 router.delete('/:id',[auth,admin],async (req, res)=>{
     const stock = await Stock.findByIdAndRemove(req.params.id);
     if(!stock)
